d7c3a6169f4adc3d96d7c3680bfe03f153b2f45e
maxikgreat <maximvasylenko228322@gmail.com>
2020-03-16 00:19:06 +0100

    Modal integrated

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,11 @@
 import {Text, View, ActivityIndicator, TouchableOpacity, Modal} from "react-native";
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from "../styles/screens/Home.component.style";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchFromDatabase} from "../redux/database/databaseAction";
 import UserRecords from "../components/Home/UserRecords";
 import {addRecord} from "../redux/database/databaseAction";
-import ModalExample from "../styles/components/ModalComp";
+import ModalComp from "../styles/components/ModalComp";
 
 
 const Home = () => {
@@ -13,41 +13,49 @@ const Home = () => {
     const dispatch = useDispatch()
     const userData = useSelector(state => state.database)
 
+    const [modalVisible, setVisible] = useState(false)
+
     useEffect(() => {
         dispatch(fetchFromDatabase())
     }, [])
 
     const showModal = () => {
-
+        setVisible(true)
     }
 
 
     return(
         <View style={styles.container}>
-            {/*{userData.isLoading*/}
-            {/*    ? <ActivityIndicator />*/}
-            {/*    : userData.error*/}
-            {/*        ? <Text>{userData.error}</Text>*/}
-            {/*        :*/}
-            {/*        <>*/}
-            {/*            <Text style={styles.headerText}>Welcome</Text>*/}
-            {/*            {userData.name ? <Text style={[styles.headerText, styles.nameText]}>{userData.name}</Text> : null}*/}
-            {/*            {userData.data*/}
-            {/*                ? <UserRecords />*/}
-            {/*                :*/}
-            {/*                <View>*/}
-            {/*                    <Text style={styles.introText}>You have no records yet. Let's fix it!</Text>*/}
-            {/*                    <TouchableOpacity*/}
-            {/*                        style={[styles.buttonContainer, styles.createRecord]}*/}
-            {/*                        onPress={() => {showModal()}}*/}
-            {/*                    >*/}
-            {/*                        <Text style={styles.addFirstText}>Add first record</Text>*/}
-            {/*                    </TouchableOpacity>*/}
-            {/*                </View>*/}
-            {/*            }*/}
-            {/*        </>*/}
-            {/*}*/}
-            <ModalExample/>
+            {userData.isLoading
+                ? <ActivityIndicator />
+                : userData.error
+                    ? <Text>{userData.error}</Text>
+                    :
+                    <>
+                        <Text style={styles.headerText}>Welcome</Text>
+                        {userData.name ? <Text style={[styles.headerText, styles.nameText]}>{userData.name}</Text> : null}
+                        {userData.data
+                            ? <UserRecords />
+                            :
+                            <View>
+                                <Text style={styles.introText}>You have no records yet. Let's fix it!</Text>
+                                <TouchableOpacity
+                                    style={[styles.buttonContainer, styles.createRecord]}
+                                    onPress={() => {showModal()}}
+                                >
+                                    <Text style={styles.addFirstText}>Add first record</Text>
+                                </TouchableOpacity>
+                            </View>
+                        }
+                    </>
+            }
+            {modalVisible
+                ? <ModalComp
+                    modalVisible = {modalVisible}
+                    setVisible = {setVisible}
+                />
+                : null
+            }
         </View>
     )
 }
--- styles/components/ModalComp.js
+++ styles/components/ModalComp.js
@@ -1,46 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Modal, Text, TouchableHighlight, View, Alert} from 'react-native';
 
-export default class ModalExample extends Component {
-    state = {
-        modalVisible: false,
-    };
+const ModalComp = ({modalVisible, setVisible}) => {
 
-    setModalVisible(visible) {
-        this.setState({modalVisible: visible});
-    }
 
-    render() {
         return (
-            <View style={{marginTop: 22}}>
                 <Modal
                     animationType="slide"
                     transparent={false}
-                    visible={this.state.modalVisible}
+                    visible={modalVisible}
                     onRequestClose={() => {
                         Alert.alert('Modal has been closed.', "Message");
                     }}>
-                    <View style={{marginTop: 22}}>
+                    <View style={{marginTop: 22, padding: 10, borderColor: "black", borderWidth: 2}}>
                         <View>
                             <Text>Hello World!</Text>
 
                             <TouchableHighlight
                                 onPress={() => {
-                                    this.setModalVisible(!this.state.modalVisible);
+                                    setVisible(!modalVisible);
                                 }}>
                                 <Text>Hide Modal</Text>
                             </TouchableHighlight>
                         </View>
                     </View>
                 </Modal>
-
-                <TouchableHighlight
-                    onPress={() => {
-                        this.setModalVisible(true);
-                    }}>
-                    <Text>Show Modal</Text>
-                </TouchableHighlight>
-            </View>
         );
-    }
 }
+
+export default ModalComp
